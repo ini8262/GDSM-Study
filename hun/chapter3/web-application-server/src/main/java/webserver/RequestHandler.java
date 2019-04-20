486941db97a0f892f7ce48b0c2355d8da989622d
ini8262 <ini8262@gmail.com>
2019-04-20 20:19:15 +0900

    3챕터 요구사항1번째 완료

---
+++ hun/chapter3/web-application-server/src/main/java/webserver/RequestHandler.java
@@ -0,0 +1,88 @@
+package webserver;
+
+import java.io.BufferedReader;
+import java.io.DataOutputStream;
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.net.Socket;
+import java.nio.file.Files;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class RequestHandler extends Thread {
+    private static final Logger log = LoggerFactory.getLogger(RequestHandler.class);
+
+    private Socket connection;
+
+    public RequestHandler(Socket connectionSocket) {
+        this.connection = connectionSocket;
+    }
+
+    public void run() {
+        log.debug("New Client Connect! Connected IP : {}, Port : {}", connection.getInetAddress(),
+                connection.getPort());
+
+        try (InputStream in = connection.getInputStream(); OutputStream out = connection.getOutputStream()) {
+            // TODO 사용자 요청에 대한 처리는 이 곳에 구현하면 된다.
+        	
+
+			//1단계
+			BufferedReader buffer = new BufferedReader(new InputStreamReader(in,"UTF-8"));
+			String line, url = null;
+			
+			int index = 0;
+			while (!"".equals(line = buffer.readLine())) {
+				if (line == null) {
+					return;
+				}
+				
+				//2단계
+				if ((index++) == 0) {
+					String[] tokens = line.split(" ");
+					url = tokens[1];
+					log.error(url);
+				}
+				
+				log.debug(line);
+			}
+			
+			//3단계
+			String fileName = "./webapp" + url;
+		    File file = new File(fileName);
+
+		    byte [] body = Files.readAllBytes(file.toPath());
+        	
+            DataOutputStream dos = new DataOutputStream(out);
+            //byte[] body = "Hello World".getBytes();
+            response200Header(dos, body.length);
+            responseBody(dos, body);
+        } catch (IOException e) {
+            log.error(e.getMessage());
+        }
+    }
+
+    private void response200Header(DataOutputStream dos, int lengthOfBodyContent) {
+        try {
+            dos.writeBytes("HTTP/1.1 200 OK \r\n");
+            dos.writeBytes("Content-Type: text/html;charset=utf-8\r\n");
+            dos.writeBytes("Content-Length: " + lengthOfBodyContent + "\r\n");
+            dos.writeBytes("\r\n");
+        } catch (IOException e) {
+            log.error(e.getMessage());
+        }
+    }
+
+    private void responseBody(DataOutputStream dos, byte[] body) {
+        try {
+            dos.write(body, 0, body.length);
+            dos.flush();
+        } catch (IOException e) {
+            log.error(e.getMessage());
+        }
+    }
+    
+}
